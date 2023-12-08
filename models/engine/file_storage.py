d27f0902903285eb6d9f44009475663d7bb7dec2
MungaSoftwiz <mwachilumobm@gmail.com>
2023-12-08 14:12:10 +0300

    Fix reload method for desirialization

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """Module containing the class FileStorage"""
 
 import json
+import os
 
 
 class FileStorage:
@@ -19,17 +20,37 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id
+        """
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
+        """
+        Serializes __objects to the JSON file
+        """
         with open(self.__file_path, 'w', encoding='utf-8') as f:
-            data = {key: value.to_dict() 
+            data = {key: value.to_dict()
                     for key, value in self.__objects.items()}
             json.dump(data, f)
 
     def reload(self):
-        try:
-            with open(self.__file_path, 'r', encoding='utf-8') as f:
+        """
+        Method desirializes the JSON file to __objects
 
-            pass
+        Raises:
+FileNotFoundError: If the json file is not found
+        """
+        from models.base_model import BaseModel
+        if os.path.exists(self.__file_path):
+            try:
+                with open(self.__file_path, 'r', encoding='utf-8') as f:
+                    dict_obj = json.load(f)
+                    for key, value in dict_obj.items():
+                        class_name = value["__class__"]
+                        class_obj = eval(class_name)
+                        obj = class_obj(**value)
+                        FileStorage.__objects[key] = obj
+            except FileNotFoundError:
+                pass
