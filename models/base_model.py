2cf846dbe80db9a768b6dd23d42857f6ff5e9bcf
Kingley Okonmah <nnamdiokonma@gmail.com>
2023-12-08 17:20:45 -0800

    fix: added else condition to handle incorrect ouput

--- models/base_model.py
+++ models/base_model.py
@@ -26,10 +26,11 @@ class BaseModel:
                     self.__dict__[key] = datetime.fromisoformat(value)
                 else:
                     self.__dict__[key] = value
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ Returns the readable string representation of an instance """
