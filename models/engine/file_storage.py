b92ce19fc75eececc4328913ba0d3efca4d6bf90
Kingley Okonmah <nnamdiokonma@gmail.com>
2023-12-09 17:18:21 -0800

    style: removed whitespace and proper visual indentation for list

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -49,7 +49,7 @@ class FileStorage:
         from models.amenity import Amenity
         from models.place import Place
         from models.review import Review
-        
+
         if os.path.exists(self.__file_path):
             try:
                 with open(self.__file_path, 'r', encoding='utf-8') as f:
